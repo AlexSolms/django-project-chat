fa5b0aaada2ff5c65cafd9f360e18ab1aaee9be9
Alexander Solms <139684886+AlexSolms@users.noreply.github.com>
2024-07-25 10:30:20 +0200

    small changes

--- chat/models.py
+++ chat/models.py
@@ -11,5 +11,5 @@ class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
-    author = models.CharField(max_length=50)
-    receiver = models.CharField(max_length=50)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')    
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
