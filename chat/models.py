cffdf18188098833adde665569d26da659255824
Alexander Solms <139684886+AlexSolms@users.noreply.github.com>
2024-07-25 07:44:55 +0200

    first models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import date
+from django.conf import settings
 
 # Create your models here.
+
+class Chat(models.Model):
+    created_at = models.DateField(default=date.today)
+    
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    created_at = models.DateField(default=date.today)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
+    author = models.CharField(max_length=50)
+    receiver = models.CharField(max_length=50)
--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
+    if request.method == 'POST':
+        print("Received data " + request.POST['textMessage'])
+    return render(request, 'chat/index.html', {'username': 'Alex'})
