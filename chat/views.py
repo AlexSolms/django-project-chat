b0f9c3c14a829fe91f9189909902bfa50a78c72a
Alexander Solms <139684886+AlexSolms@users.noreply.github.com>
2024-07-26 16:49:47 +0200

    only backup

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Chat, Message
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def index(request):
@@ -11,3 +13,12 @@ def index(request):
     chatMessages = Message.objects.filter(chat__id=1)
     
     return render(request, 'chat/index.html', {'messages': chatMessages})
+
+def login_view(request):
+    if request.method == 'POST':
+        user = ...
+        if user:
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True})
+    return render(request, 'auth/login.html')
